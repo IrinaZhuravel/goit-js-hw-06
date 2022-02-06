a1147b7104a555515166fae285ff9e88d6fb8eee
IrinaZhuravel <90681450+IrinaZhuravel@users.noreply.github.com>
2022-02-06 13:26:42 +0200

    task2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const ingredientListEl = document.querySelector('#ingredients');
+
+const ingredientListItemEl = ingredients.map((element)=>{
+      const listItemEl = document.createElement('li');
+      listItemEl.textContent = element;
+      listItemEl.classList.add('item');
+      return listItemEl;
+});
+
+ingredientListEl.append(...ingredientListItemEl);
+
