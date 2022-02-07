3c4f1acd0452bde2fa09bf9fffc2808c20453c27
IrinaZhuravel <90681450+IrinaZhuravel@users.noreply.github.com>
2022-02-07 14:19:21 +0200

    task9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
+const colorSpanEl = document.querySelector('span.color');
+const changeColorButtonEl = document.querySelector('button.change-color');
+
+changeColorButtonEl.addEventListener('click', onColorChangeClick);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onColorChangeClick() {
+  colorSpanEl.textContent = getRandomHexColor();
+  document.body.style.backgroundColor = getRandomHexColor();
 }
