95d6c8db0a0ca1e3f08b212689cd52e0eee1841c
IrinaZhuravel <90681450+IrinaZhuravel@users.noreply.github.com>
2022-02-07 17:46:24 +0200

    Task 10 v 2

--- js/task-10.js
+++ js/task-10.js
@@ -1,37 +1,33 @@
-const inputEl = document.querySelector('input')
-const createBtnEl = document.querySelector('[data-create]');
-const destroyBtnRef = document.querySelector('[data-destroy]');
-const boxesEl = document.querySelector('#boxes');
 
-createBtnEl.addEventListener('click', onCreateBtnClick);
-destroyBtnRef.addEventListener('click', onDestroyBtnClick);
-
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
-
-
-function onDestroyBtnClick() {
-  boxesEl.innerHTML = "";
-  inputEl.value = "";
-}
-
-function createBoxes(amount) {
-  let size = 30;
-  const array = [];
-  for (const i = 1; i <= amount; i += 1) {
-    const color = getRandomHexColor();
-    array.push(
-      `<div class = "box" style="width: ${size}px; height: ${size}px; background-color: ${color}"></div>`,
-    );
-    size += 10;
-  }
-  boxesEl.insertAdjacentHTML('beforeend', arr.join(''));
-}
-
-function onCreateBtnClick() {
-  const quantity = inputEl.value;
-  quantity ? createBoxes(amount) : alert("Введите число");
-  inputEl.value = "";
-}
-
+  
+ const inputEl = document.querySelector('input');
+ const createBtnEl = document.querySelector('[data-create]');
+ const destroyBtnRef = document.querySelector('[data-destroy]');
+ const boxesEl = document.querySelector('#boxes');
+ 
+ createBtnEl.addEventListener('click', () => {
+     createBoxes(inputEl.value);
+ });
+ destroyBtnRef.addEventListener('click', onDestroyBtnClick);
+ 
+ function getRandomHexColor() {
+     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+ }
+ 
+ function onDestroyBtnClick() {
+     boxesEl.innerHTML = '';
+     inputEl.value = '';
+ }
+ 
+ function createBoxes(amount) {
+     let size = 30;
+     const array = [];
+     for (let i = 1; i <= amount; i += 1) {
+         const color = getRandomHexColor();
+         array.push(
+             `<div class = "box" style="width: ${size}px; height: ${size}px; background-color: ${color}"></div>`,
+         );
+         size += 10;
+     }
+     boxesEl.insertAdjacentHTML('beforeend', array.join(''));
+ }
