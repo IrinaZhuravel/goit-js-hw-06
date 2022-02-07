1d8f17b4c0bb27f5d512d238dc624fec002dbe74
IrinaZhuravel <90681450+IrinaZhuravel@users.noreply.github.com>
2022-02-07 12:58:27 +0200

    task6

---
+++ js/task-06.js
@@ -0,0 +1,10 @@
+const input = document.querySelector('#validation-input');
+
+input.addEventListener('blur', onInputBlur);
+
+function onInputBlur() {
+  if (Number(input.dataset.length) === input.value.trim().length) {
+    input.setAttribute('class', 'valid');
+  }
+  else { input.setAttribute('class', 'invalid');}
+}
