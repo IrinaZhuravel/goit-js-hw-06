4f1dd6f3abebb9cfdc0901c7ef2f52f038dd0f03
IrinaZhuravel <90681450+IrinaZhuravel@users.noreply.github.com>
2022-02-07 13:44:35 +0200

    task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,29 @@
 
+const form = document.querySelector('.login-form');
+
+form.addEventListener('submit', onBtnSubmit);
+
+function onBtnSubmit(event) {
+  event.preventDefault();
+
+  const formEl = event.currentTarget.elements;
+  const mail = formEl.email.value;
+  const password = formEl.password.value;
+
+  if (mail === "" || password === "") {
+    alert("Заполнены не все поля ввода");
+    form.reset();
+    return;
+  }
+  const authorizationFormData = {
+    site: location.href,
+  };
+
+  const formData = new FormData(event.currentTarget);
+  formData.forEach((value, name) => {
+    authorizationFormData[name] = value;
+  });
+  console.log(authorizationFormData);
+
+  form.reset;
+}
