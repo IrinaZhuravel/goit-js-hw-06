527e8717095a4c4ae07ee496c802464bd88bafad
IrinaZhuravel <90681450+IrinaZhuravel@users.noreply.github.com>
2022-02-08 09:22:16 +0200

    task9

--- js/task-09.js
+++ js/task-09.js
@@ -1,13 +1,29 @@
-const colorSpanEl = document.querySelector('span.color');
-const changeColorButtonEl = document.querySelector('button.change-color');
+//const colorSpanEl = document.querySelector('span.color');
+//const changeColorButtonEl = document.querySelector('button.change-color');
 
-changeColorButtonEl.addEventListener('click', onColorChangeClick);
+//changeColorButtonEl.addEventListener('click', onColorChangeClick);
+
+//function getRandomHexColor() {
+  //return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+//}
+
+//function onColorChangeClick() {
+  //colorSpanEl.textContent = getRandomHexColor();
+  //document.body.style.backgroundColor = getRandomHexColor();
+//}
+const changeColorButtonEl = document.querySelector('button.change-color');
+const bodyEl = document.body;
 
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
 
-function onColorChangeClick() {
-  colorSpanEl.textContent = getRandomHexColor();
-  document.body.style.backgroundColor = getRandomHexColor();
+function onColorChangeClick () {
+  
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  document.querySelector('.color').innerHTML = bodyEl.style.backgroundColor;
+  
 }
+changeColorButtonEl.addEventListener('click', onColorChangeClick);
