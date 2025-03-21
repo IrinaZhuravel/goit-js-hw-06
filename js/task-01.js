1bd832c2327a91f4c75a73608e2d613ccd22002d
IrinaZhuravel <90681450+IrinaZhuravel@users.noreply.github.com>
2025-03-21 19:16:32 +0200

    task 1 refactoring

--- js/task-01.js
+++ js/task-01.js
@@ -4,31 +4,11 @@ const categories = document.querySelectorAll(".item");
 console.log("Number of categories: ", categories.length) 
   
 categories.forEach((item) => {
-        const category = item.firstElementChild;
-        const elements = item.children[1].children;
-        console.log("Category: ", category.textContent)
-        console.log("Elements: ", elements.length)
+        const category = item.querySelector("h2").textContent;
+        const elements = item.querySelectorAll("li").length;
+        console.log("Category: ", category)
+        console.log("Elements: ", elements)
 })
 
 
 
-/*
-Напиши скрипт который:
-
-Посчитает и выведет в консоль количество категорий в ul#categories, то есть элементов li.item.
-Для каждого элемента li.item в списке ul#categories, найдет и выведет в консоль текст заголовка элемента (тега <h2>) и количество элементов в категории (всех вложенных в него <li>).
-Для выполнения этой задачи нужно использовать метод forEach() и свойства навигации по DOM.
-
-В результате, в консоли будут выведены такие сообщения.
-
-Number of categories: 3
-
-Category: Animals
-Elements: 4
-
-Category: Products
-Elements: 3
-
-Category: Technologies
-Elements: 5
-*/ 
