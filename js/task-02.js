64f3ebdf97e7e43af9701f14dd7ec2ec620423e3
IrinaZhuravel <90681450+IrinaZhuravel@users.noreply.github.com>
2025-03-21 20:40:22 +0200

    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,22 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.getElementById("ingredients");
+
+const ingredientItems = ingredients.map((ingredient) => {
+  const li = document.createElement("li");
+  li.textContent = ingredient;
+  li.classList.add("item");
+  return li;
+})
+
+ingredientsList.append(...ingredientItems)
+
+// ingredients.forEach((ingredient) => {
+//   const ingredientItem = document.createElement("li"); 
+//   ingredientItem.textContent = ingredient;
+//   ingredientItem.classList.add("item");
+//   ingredientsList.append(ingredientItem)
+// })
+
